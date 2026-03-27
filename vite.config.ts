4b5e208595ac86eb2fe1dd304d7d9f16ba89c542
EricTorresAndrade <eric.torres@nautilus.team>
2026-03-26 19:54:49 -0600

    chore: Disable ngrok plugin in vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
-import { ngrok } from 'vite-plugin-ngrok'
+// import { ngrok } from 'vite-plugin-ngrok'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react(), svgr(), ngrok({
-    compression: true,
-    authtoken: '2pHekMvnS5S75v3oiFFQICtQixf_5jGyJHcdXFXaPga1YL1pj',
-  })],
+  plugins: [react(), svgr(),
+    //    ngrok({
+    //   compression: true,
+    //   authtoken: '2pHekMvnS5S75v3oiFFQICtQixf_5jGyJHcdXFXaPga1YL1pj',
+    // })
+  ],
 })
