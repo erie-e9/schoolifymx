43fba7307017398ee7c1850d28590bf65608c026
EricTorresAndrade <eric.torres@nautilus.team>
2026-04-06 08:22:25 -0600

    chore: update vite base path to /schoolifymx/

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/schoolifymx/',
   plugins: [react(), svgr()],
   resolve: {
     alias: {
