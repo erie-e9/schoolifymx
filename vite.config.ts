a8608bfde3c8dcf61f8afde1875c3e218f9496d4
EricTorresAndrade <eric.torres@nautilus.team>
2026-04-06 08:25:17 -0600

    chore: update vite base path to relative directory for deployment compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/schoolifymx/',
+  base: './',
   plugins: [react(), svgr()],
   resolve: {
     alias: {
